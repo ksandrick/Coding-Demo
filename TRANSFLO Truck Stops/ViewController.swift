303b5f4b8d01ba0a9ac77217f2d47e70a0f3fbb9
Kris Sandrick <kristopher.sandrick@silvercar.com>
2018-04-25 13:12:10 -0400

    Initial service call setup.

--- TRANSFLO Truck Stops/ViewController.swift
+++ TRANSFLO Truck Stops/ViewController.swift
@@ -12,12 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        retrieveTruckStops()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    fileprivate func retrieveTruckStops() {
+        let url = URL(string: "http://webapp.transflodev.com/svc1.transflomobile.com/api/v3/stations/10")!
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let apiKey = "amNhdGFsYW5AdHJhbnNmbG8uY29tOnJMVGR6WmdVTVBYbytNaUp6RlIxTStjNmI1VUI4MnFYcEVKQzlhVnFWOEF5bUhaQzdIcjVZc3lUMitPTS9paU8="
+        request.setValue("Basic \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        let json: [String: Any] = ["lat": 36.665115,
+                                   "lng": -121.636536]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for general errors
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
     }
 
 
