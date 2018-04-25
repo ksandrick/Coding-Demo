4cdcdd4ce5c4ad43211dd23f4a62bfdf3638c959
Kris Sandrick <kristopher.sandrick@silvercar.com>
2018-04-25 17:58:24 -0400

    Added and migrated to constants from string literals and hard-coded values

---
+++ TRANSFLO Truck Stops/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TRANSFLO Truck Stops
+//
+//  Created by Kristopher Sandrick on 4/25/18.
+//  Copyright © 2018 Kristopher Sandrick. All rights reserved.
+//
+
+import Foundation
+
+struct Services {
+    static let apiURL = "http://webapp.transflodev.com/svc1.transflomobile.com/api/v3/stations/"
+    static let apiKey = "amNhdGFsYW5AdHJhbnNmbG8uY29tOnJMVGR6WmdVTVBYbytNaUp6RlIxTStjNmI1VUI4MnFYcEVKQzlhVnFWOEF5bUhaQzdIcjVZc3lUMitPTS9paU8="
+}
--- TRANSFLO Truck Stops/ViewController.swift
+++ TRANSFLO Truck Stops/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    static let defaultRadius: CGFloat = 100.0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,30 +23,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    fileprivate func retrieveTruckStops() {
-        let url = URL(string: "http://webapp.transflodev.com/svc1.transflomobile.com/api/v3/stations/10")!
+    fileprivate func retrieveTruckStops(radius: Double = 100.0,
+                                        location: CLLocation) {
+        let urlString = Services.apiURL + String(format:"%f", radius)
+        let url = URL(string: urlString)!
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let apiKey = "amNhdGFsYW5AdHJhbnNmbG8uY29tOnJMVGR6WmdVTVBYbytNaUp6RlIxTStjNmI1VUI4MnFYcEVKQzlhVnFWOEF5bUhaQzdIcjVZc3lUMitPTS9paU8="
-        request.setValue("Basic \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.setValue("Basic \(Services.apiKey)", forHTTPHeaderField: "Authorization")
         request.httpMethod = "POST"
-        let json: [String: Any] = ["lat": 36.665115,
-                                   "lng": -121.636536]
+        let json: [String: Any] = ["lat": location.coordinate.latitude,
+                                   "lng": location.coordinate.longitude]
         let jsonData = try? JSONSerialization.data(withJSONObject: json)
         request.httpBody = jsonData
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {                                                 // check for general errors
+            guard let data = data, error == nil else {
+                // check for general errors
                 print("error=\(String(describing: error))")
                 return
             }
             
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(String(describing: response))")
             }
             
-            let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(String(describing: responseString))")
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: [])
+                print("Result -> \(describing: results)")
+                
+            } catch {
+//                print("Error -> \(describing: error)")
+            }
         }
         task.resume()
     }
