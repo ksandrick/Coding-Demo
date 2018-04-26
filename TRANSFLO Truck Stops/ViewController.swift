7d7ff4859ba1863f4753d3b5d27dbaf6684f4b89
Kris Sandrick <kristopher.sandrick@silvercar.com>
2018-04-25 22:16:00 -0400

    Initial add of map view and annotations

--- TRANSFLO Truck Stops/ViewController.swift
+++ TRANSFLO Truck Stops/ViewController.swift
@@ -8,26 +8,48 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
     
-    static let defaultRadius: CGFloat = 100.0;
+    var truckStops: [TruckStop] = [] {
+        didSet {
+            for truckStop in truckStops {
+                print(truckStop.name)
+                self.mapView.addAnnotation(truckStop)
+            }
+        }
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+//    static let defaultRadius: CGFloat = 100.0;
+    let regionRadius: CLLocationDistance = 100
+    let initialLocation = CLLocation(latitude: 36.665115, longitude: -121.636536)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        centerMapOnLocation(location: initialLocation)
+        
         let latitude: CLLocationDegrees = 36.665115
         let longitude: CLLocationDegrees = -121.636536
         let searchLocation = CLLocation.init(latitude: latitude, longitude: longitude)
+        
         TruckStop.retrieveTruckStops(location: searchLocation) { truckStops in
-            for truckStop in truckStops {
-                print(truckStop.name)
-            }
+            self.truckStops = truckStops
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
 }
