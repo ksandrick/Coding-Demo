7d221eca2de0e09efd1e82e8c8586c3f4c99af24
Kris Sandrick <kristopher.sandrick@silvercar.com>
2018-04-29 12:18:31 -0400

    Added location manager, initial request for location services, initial map view, and rudimentary map pins and annotations

--- TRANSFLO Truck Stops/ViewController.swift
+++ TRANSFLO Truck Stops/ViewController.swift
@@ -10,35 +10,31 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     var truckStops: [TruckStop] = [] {
         didSet {
             for truckStop in truckStops {
                 print(truckStop.name)
-                self.mapView.addAnnotation(truckStop)
+                self.mapView.addAnnotations(truckStops)
             }
         }
     }
     
+    var locationManager:CLLocationManager!
     @IBOutlet weak var mapView: MKMapView!
     
-//    static let defaultRadius: CGFloat = 100.0;
-    let regionRadius: CLLocationDistance = 100
-    let initialLocation = CLLocation(latitude: 36.665115, longitude: -121.636536)
+    let defaultRadius: CLLocationDistance = 100.0
+    let metersInAMile: Double = 1609.344
+    let milesInAMeter: Double = 0.000621371192
+//    let regionRadius: CLLocationDistance = 100
+//    let initialLocation = CLLocation(latitude: 36.665115, longitude: -121.636536)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        centerMapOnLocation(location: initialLocation)
-        
-        let latitude: CLLocationDegrees = 36.665115
-        let longitude: CLLocationDegrees = -121.636536
-        let searchLocation = CLLocation.init(latitude: latitude, longitude: longitude)
-        
-        TruckStop.retrieveTruckStops(location: searchLocation) { truckStops in
-            self.truckStops = truckStops
-        }
+        mapView.delegate = self
+        determineLocation()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,11 +42,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func determineLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.startUpdatingLocation()
+            }
+        }
+    }
+    
     func centerMapOnLocation(location: CLLocation) {
+        let radiusInMiles = metersInAMile * defaultRadius
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                  regionRadius, regionRadius)
+                                                                  radiusInMiles, radiusInMiles)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+        let userLocation:CLLocation = locations[0] as CLLocation
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        centerMapOnLocation(location: userLocation)
+        TruckStop.retrieveTruckStops(location: userLocation) { truckStops in
+            self.truckStops = truckStops
+        }
+    }
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        guard let annotation = annotation as? TruckStop else { return nil }
+        let identifier = "truckStopMarker"
+        var view: MKMarkerAnnotationView
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            //view.image = UIImage(named: "truckPin")
+        }
+        return view
+    }
+    
+}
