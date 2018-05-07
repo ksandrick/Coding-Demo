9bf1343525fbfb9ddb841de1fdd21bb4d99a096f
Kris Sandrick <kristopher.sandrick@silvercar.com>
2018-05-07 14:06:58 -0400

    Addition of UI for search VC

---
+++ TRANSFLO Truck Stops/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  TRANSFLO Truck Stops
+//
+//  Created by Kristopher Sandrick on 5/7/18.
+//  Copyright © 2018 Kristopher Sandrick. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+
+
+}
